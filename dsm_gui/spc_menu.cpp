aefa6c6fcbcf6f3ae4681919e91e6edd601cda21
Jerry Varghese <jerry.j.varghese@nasa.gov>
2023-06-14 15:49:24 -0400

    Started coding logic for SPC_Menu based on ORB_Menu and FOV_Menu

--- dsm_gui/spc_menu.cpp
+++ dsm_gui/spc_menu.cpp
@@ -1,14 +1,195 @@
-#include "spc_menu.h"
-#include "ui_spc_menu.h"
+#include "SPC_Menu.h"
+#include "ui_SPC_Menu.h"
 
-SPC_menu::SPC_menu(QWidget *parent) :
+#include <QFile>
+#include <QTextStream>
+#include <QMessageBox>
+#include <QRegularExpression>
+#include <QDir>
+
+SPC_Menu::SPC_Menu(QWidget *parent) :
     QDialog(parent),
-    ui(new Ui::SPC_menu)
+    ui(new Ui::SPC_Menu)
 {
     ui->setupUi(this);
 }
 
-SPC_menu::~SPC_menu()
+SPC_Menu::~SPC_Menu()
 {
     delete ui;
 }
+
+
+
+void SPC_Menu::on_spc_add_clicked()
+{
+
+}
+
+
+void SPC_Menu::on_spc_remove_clicked()
+{
+
+}
+
+
+void SPC_Menu::on_spc_duplicate_clicked()
+{
+
+}
+
+
+void SPC_Menu::on_spc_load_clicked()
+{
+
+}
+
+
+void SPC_Menu::on_spc_save_clicked()
+{
+
+}
+
+
+void SPC_Menu::on_spc_close_clicked()
+{
+
+}
+
+
+void SPC_Menu::on_spc_apply_clicked()
+{
+
+}
+
+
+void SPC_Menu::on_spc_conf_clicked()
+{
+
+}
+
+
+QString SPC_Menu::whitespace(QString data)
+{
+    QString empty_space = "                              ";
+    int data_len = empty_space.count()-data.count();
+    if (data_len < 1) data_len = 1;
+    for (int i = 0; i < data_len; i++){
+        data.append(" ");
+    }
+    return data;
+}
+
+
+void SPC_Menu::on_spc_list_itemClicked(QListWidgetItem *item)
+{
+     int index = ui->spc_list->row(item);
+
+    if (index == global_spc_index) {
+        return;
+    }
+    else {
+        if ( (global_spc_index != -1) && (global_spc_ignore == 0) ) {
+            int response = warning_message("Note that changes to the previous selected spacecraft are lost unless you first select \"Apply\"! This is your only warning.");
+            if (response == QMessageBox::Cancel) {
+                ui->spc_list->setCurrentRow(global_spc_index);
+                global_spc_ignore = 1;
+                return;
+            }
+            else if (response == QMessageBox::Ok) {
+                global_spc_ignore = 1;
+            }
+        }
+    }
+
+    receive_data();
+    apply_data();
+
+    global_spc_index = index;
+
+
+}
+
+void SPC_Menu::receive_data()
+{
+    // Daniel's regex from ORB_Menu
+    // Return everything up to and including ! (exclamation point)
+    static QRegularExpression rx1("(.*?)!");
+
+    // Return everything between a set of " " (quotation marks)
+    static QRegularExpression rx2("\"(.*?)\"");
+
+    // If the line does NOT start with an alphanumeric character or " (single quotation), then return the line as first group.
+    // Otherwise return everything after ! (exclamation point) as second group
+    static QRegularExpression rx3("(?:(?=^[^[:alnum:]|\"])([^[:alnum:]|\"].*)|(!.*))");
+
+    QFile file(file_path);
+    if(!file.open(QIODevice::ReadOnly)) {
+        QMessageBox::information(0, "error", file.errorString());
+    }
+
+    QTextStream in(&file);
+    while(!in.atEnd()) {
+        QString line = in.readLine();
+        QRegularExpressionMatch match1 = rx1.match(line);
+        spc_data.append(match1.captured(1)); // index 0 includes ! character, index 1 does not
+
+        QRegularExpressionMatch match2 = rx2.match(line);
+        spc_string.append(match2.captured(1)); // index 0 includes "" characters, index 1 does not
+
+        //        line.append("\n");
+        QRegularExpressionMatch match3 = rx3.match(line);
+        if (match3.hasMatch()) {
+            QString capture = match3.captured(1);
+            if (!capture.isEmpty()) capture += "\n";
+            spc_file_headers.append(capture);
+            capture = match3.captured(2);
+            if (!capture.isEmpty()) capture += "\n";
+            spc_file_descrip.append(capture);
+        }
+
+    }
+    file.close();
+
+}
+
+void SPC_Menu::apply_data()
+{
+
+}
+
+void SPC_Menu::receive_spcpath(QString path)
+{
+    inout_path = path;
+
+    QStringList spcFiles = QDir(inout_path).entryList({"SC_*"});
+    for (int i = 0; i<spcFiles.length(); i++) {
+        file_paths.append(inout_path+spcFiles[i]); // Full file path of SC file
+        spc_names.append(spcFiles[i].chopped(4).mid(4)); // Everything between "SC_" and ".txt"
+    }
+
+    file_paths.sort();
+    spc_names.sort();
+
+    ui->spc_list->clear();
+    for(int i=0; i<file_paths.length();i++) {
+        file_path = file_paths[i];
+        spc_name_index = i;
+
+        receive_data();
+        apply_data();
+    }
+    ui->spc_list->addItems(spc_names);
+
+}
+
+int SPC_Menu::warning_message(QString warningText)
+{
+    QMessageBox warningMsg;
+    warningMsg.setIcon(QMessageBox::Warning);
+    warningMsg.setText(warningText);
+    warningMsg.setStandardButtons(QMessageBox::Ok | QMessageBox::Cancel);
+    int ret = warningMsg.exec();
+    return ret;
+}
+
--- dsm_gui/spc_menu.h
+++ dsm_gui/spc_menu.h
@@ -1,22 +1,67 @@
-#ifndef SPC_MENU_H
-#define SPC_MENU_H
+#ifndef SPC_Menu_H
+#define SPC_Menu_H
 
 #include <QDialog>
+#include <QListWidgetItem>
 
 namespace Ui {
-class SPC_menu;
+class SPC_Menu;
 }
 
-class SPC_menu : public QDialog
+class SPC_Menu : public QDialog
 {
     Q_OBJECT
 
 public:
-    explicit SPC_menu(QWidget *parent = nullptr);
-    ~SPC_menu();
+    explicit SPC_Menu(QWidget *parent = nullptr);
+    ~SPC_Menu();
+
+private slots:
+
+    void on_spc_add_clicked();
+    void on_spc_remove_clicked();
+    void on_spc_duplicate_clicked();
+
+    void on_spc_load_clicked();
+    void on_spc_save_clicked();
+    void on_spc_close_clicked();
+    void on_spc_apply_clicked();
+
+    void on_spc_conf_clicked();
+
+    QString whitespace(QString);
+
+    void on_spc_list_itemClicked(QListWidgetItem *item);
+
+    void receive_data();
+    void apply_data();
+    void populate_list();
+    void write_data();
+
+    int warning_message(QString);
+
+    void receive_spcpath(QString);
 
 private:
-    Ui::SPC_menu *ui;
+    Ui::SPC_Menu *ui;
+
+    int global_spc_index = -1;
+    int global_spc_ignore = 0;
+
+    QString inout_path;
+    QString file_path;
+    QStringList file_paths;
+
+
+    QStringList spc_names;
+    QStringList spc_string;
+    QStringList spc_data;
+    int spc_name_index;
+
+    QStringList spc_file_headers; // section headers in the file
+    QStringList spc_file_descrip; // data descriptors in the file
+
+
 };
 
-#endif // SPC_MENU_H
+#endif // SPC_Menu_H
